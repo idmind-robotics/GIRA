74b49b988a716f9d129bd6a1cdcbcab8fa65af8f
TiagoMRPJ <147532464+TiagoMRPJ@users.noreply.github.com>
2024-03-08 14:42:51 +0000

    Delete mainOTA directory

--- mainOTA/README.md
+++
@@ -1,5 +0,0 @@
-make erase # Erase ESP32
-
-make image # Install MicroPython on ESP32
-
-make rsync # Copy the files that are different
--- mainOTA/src/boot.py
+++
@@ -1,6 +0,0 @@
-import machine
-from machine import Pin
-
-#sound the buzzer to indicate the device is booting
-buzzer = machine.PWM(machine.Pin(14), freq = 800, duty = 0)
-buzzer.deinit()
